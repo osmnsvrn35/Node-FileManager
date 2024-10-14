92e662bd68bb8c7e26ac0d4db302e0092864b7e4
Osman Can Savran <104326059+osmnsvrn35@users.noreply.github.com>
2024-10-14 23:42:44 +0200

    feat: add cp  operation

--- src/fileOperations.js
+++ src/fileOperations.js
@@ -1,7 +1,8 @@
 import { stat, readFile, open, rename } from 'node:fs/promises';
 import { EOL } from 'node:os';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { promises as fsPromises } from 'node:fs';
 import path from 'path';
-
 export async function cat(filePath) {
     
     const absolutePath = path.isAbsolute(filePath)
@@ -19,8 +20,33 @@ export async function add(fileName) {
 }
 
 export async function rn(oldPath, newFileName) {
-  
     const absoluteOldPath = path.resolve(process.cwd(), oldPath);
     const newFilePath = path.resolve(path.dirname(absoluteOldPath), newFileName);
     await rename(absoluteOldPath, newFilePath);
+}
+
+export async function cp(sourcePath, destinationDir) {
+
+    const absoluteSourcePath = path.resolve(process.cwd(), sourcePath);
+  
+    const absoluteDestinationDir = path.resolve(process.cwd(), destinationDir);
+    await fsPromises.access(absoluteDestinationDir); 
+  
+
+    const destinationFilePath = path.join(absoluteDestinationDir, path.basename(absoluteSourcePath));
+  
+    if (absoluteSourcePath === destinationFilePath) {
+      throw new Error("Source and destination paths are the same.");
+    }
+  
+    const readableStream = createReadStream(absoluteSourcePath);
+    const writableStream = createWriteStream(destinationFilePath);
+
+    readableStream.pipe(writableStream);
+  
+    return new Promise((resolve, reject) => {
+      writableStream.on('finish', resolve);
+      writableStream.on('error', reject);  
+      readableStream.on('error', reject);  
+    });
   }
